5237e706a51806711a5f2d67a04ea191b29ee8e2
john <jhorn@dstribune.com>
2016-02-23 23:05:51 -0800

    converted downloaded files into temp in memory operations for water data

--- FlintAnalysis.R
+++ FlintAnalysis.R
@@ -1,26 +1,98 @@
 
 setwd("~/DSTribune/Stories/FlintWaterQuality")
 library(ggplot2)
-library(acs)
 library(dplyr)
-library(gdal)
-library(maptools)
 
 
-#download.file("http://waterqualitydata.us/Result/search?countrycode=US&statecode=US%3A26&countycode=US%3A26%3A049&sampleMedia=Water&characteristicType=Inorganics%2C+Major%2C+Non-metals&characteristicName=Chloride&mimeType=csv&zip=yes&sorted=no", destfile = "waterQualityMI")
-wqDf <- read.csv("result.csv")
-wqDf <- filter(wqDf, ActivityMediaSubdivisionName == "Surface Water", ResultSampleFractionText == 'Dissolved', ResultStatusIdentifier == 'Accepted')
+temp <- tempfile()
+download.file("http://waterqualitydata.us/Result/search?countrycode=US&statecode=US%3A26&countycode=US%3A26%3A049&sampleMedia=Water&characteristicType=Inorganics%2C+Major%2C+Non-metals&characteristicName=Chloride&mimeType=csv&zip=yes&sorted=no", temp)
+wqGen<- read.csv(unz(temp, "result.csv"))
+wqGen$County = "Genesee"
+
+#The water planned to be sourced from Lake Huron is located in Sanilac County
+#Source: http://www.nytimes.com/2014/05/26/business/detroit-plan-to-profit-on-water-looks-half-empty.html?_r=0
+
+temp <- tempfile()
+download.file("http://waterqualitydata.us/Result/search?countrycode=US&statecode=US%3A26&countycode=US%3A26%3A151&sampleMedia=Water&characteristicType=Inorganics%2C+Major%2C+Non-metals&characteristicName=Chloride&mimeType=csv&zip=yes&sorted=no", temp)
+wqSanilac<- read.csv(unz(temp, "result.csv"))
+wqSanilac$County = "Sanilac"
+
+
+#The majority of Detroits water is sourced from the Detroit River in Wayne County
+#Source: http://www.dwsd.org/downloads_n/customer_service/customer_information/water_quality_report.pdf
+
+temp <- tempfile()
+download.file("http://waterqualitydata.us/Result/search?countrycode=US&statecode=US%3A26&countycode=US%3A26%3A163&sampleMedia=Water&characteristicType=Inorganics%2C+Major%2C+Non-metals&characteristicName=Chloride&mimeType=csv&zip=yes&sorted=no", temp)
+wqWayne<- read.csv(unz(temp, "result.csv"))
+wqWayne$County = "Wayne"
+
+#Merge the three County Water Measurements
+wqDf <- rbind(wqGen, wqSanilac, wqWayne)
+
+wqDf <- filter(wqDf, ActivityMediaSubdivisionName == "Surface Water", ResultSampleFractionText == 'Dissolved', ResultStatusIdentifier == 'Accepted' | ResultStatusIdentifier == 'Final' | ResultStatusIdentifier == 'Historical')
 wqDf$MonitoringLocationIdentifier <- as.character(wqDf$MonitoringLocationIdentifier)
 wqDf$ActivityStartDate <- as.POSIXct(wqDf$ActivityStartDate)
 wqDf <- wqDf %>%
   filter(ResultMeasureValue != "NA")
 
-summary(wqDf$ResultMeasureValue)
+#What we want is a percentage of samples binned by concentration
+percentConc<- wqDf %>%
+  group_by(County) %>%
+  summarise(Avg = mean(ResultMeasureValue, na.rm = TRUE),
+            Max = max(ResultMeasureValue, na.rm = TRUE),
+            LatestSample = max(ActivityStartDate, na.rm = TRUE),
+            totalSamples = n(),
+            stdError = sd(ResultMeasureValue, na.rm = TRUE))
 
-ggplot(data=wqDf, aes(ResultMeasureValue)) + geom_histogram(color = "black", fill = 'blue') + ggtitle("Distribution of surface water dissolved chloride \n in Genesee County, USGS NAWQA") + ylab("Frequency") + xlab("mg/l Dissolved Chloride")
 
-#download.file("http://waterqualitydata.us/Result/search?countrycode=US&statecode=US%3A26&countycode=US%3A26%3A049&mimeType=csv&zip=yes&sorted=no", destfile = "stationLocationsMI")
-#download.file("http://waterqualitydata.us/Station/search?countrycode=US&statecode=US%3A26&countycode=US%3A26%3A049&mimeType=kml&zip=yes&sorted=no", destfile = "stationShapesMI")
+ggplot(data=percentConc, aes(x=County, y=Avg)) + 
+         stat_boxplot(geom ='errorbar') + 
+         geom_boxplot()
+
+percentConc$min <- percentConc$Avg - percentConc$stdError
+percentConc$max <- percentConc$Avg + percentConc$stdError
+       
+plot1 <- ggplot(percentConc, aes(x=County)) 
+plot1 <- plot1 + geom_errorbar(aes(ymin=min,ymax=max),data=percentConc,width = 0.5)
+plot1 <- plot1 + geom_boxplot(aes(y=Avg))
+plot1 <- plot1 + ggtitle("Surface Water Chloride Concentrations \n in Genesse, Sanilac, and Wayne County MI (USGS)") + ylab("Average Chloride Concentration")
+plot1
+
+#Just looking initially at potential corrosivity of initial surface water across the three counties it seems that Wayne County which contains the Detroit River may have a slight
+# advantage because initial concentrations of chloride are lower
+
+Gen <- filter(wqDf, County == "Genesee")
+San <- filter(wqDf, County == "Sanilac")
+Way <- filter(wqDf, County == "Wayne")
+
+Gen_San <- t.test(Gen$ResultMeasureValue, San$ResultMeasureValue)
+Gen_Way <- t.test(Gen$ResultMeasureValue, Way$ResultMeasureValue)
+San_Way <- t.test(San$ResultMeasureValue, Way$ResultMeasureValue)
+
+Gen_San$p.value
+Gen_Way$p.value
+San_Way$p.value
+
+#Indeed initial untreated surface water concentrations of chloride are significantly lower in Wayne County compared to Sanilac and Genesee
+
+#We should get more granular now and display these samples relative to the three sites rather than entire counties
+
+#Wayne KML
+temp <- tempfile()
+download.file("http://waterqualitydata.us/Station/search?countrycode=US&statecode=US%3A26&countycode=US%3A26%3A163&mimeType=kml&zip=yes&sorted=no", temp)
+genStations<- readLines(unz(temp, "station.kml"))
+
+
+download.file("http://waterqualitydata.us/Station/search?countrycode=US&statecode=US%3A26&countycode=US%3A26%3A163&mimeType=kml&zip=yes&sorted=no", destfile = "wayneStationCoords")
+      
+#Sanilac KML
+download.file("http://waterqualitydata.us/Station/search?countrycode=US&statecode=US%3A26&countycode=US%3A26%3A151&mimeType=kml&zip=yes&sorted=no", destfile = "sanilacStationCoords")
+
+#Genesee KML
+download.file("http://waterqualitydata.us/Station/search?countrycode=US&statecode=US%3A26&countycode=US%3A26%3A049&mimeType=kml&zip=yes&sorted=no", destfile = "geneseeStationCoords")
+unzip(zipfile='geneseeStationCoords')
+
+
 
 sampleData <- wqDf %>%
   group_by(MonitoringLocationIdentifier) %>%
@@ -31,8 +103,12 @@ sampleData <- wqDf %>%
 print(sampleData)
 
 #At this point we used QGIS to open the kml and extract the coordinates for these 5 unique sample locations
-Lat = c(-83.79, -83.7902778, -83.8460698, -83.6693998, -83.4627282)
-Long = c(42.9111111, 42.9119444, 42.9419733, 43.0003052, 43.1144707)
-MonitoringLocationIdentifier = c("USGS-0414826544", "USGS-0414826545", "USGS-041482663", "USGS-430001083401006", "USGS-430645083274606")
-coords <- data.frame(Lat, Long, MonitoringLocationIdentifier)
+stationLocations <- read.csv(file="stationLocationsMI.csv")
+
+
 coords <- merge(x=coords, y=sampleData, by="MonitoringLocationIdentifier", all = TRUE)
+names(coords) <-  c("ID", "Lat", "Long", "Average [Cl-] (mg/l)", "Max [Cl-] (mg/l)", "Latest Sample", "Total Samples ")
+write.csv(coords,file="GeneseeSamples.csv")
+
+
+
